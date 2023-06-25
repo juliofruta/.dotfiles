015985156b5d1598488596c41de4b8180539189a
Julio Fruta <julio.guzman@me.com>
2023-06-25 10:46:04 -0700

    wip

--- dotfiles.swiftpm/MyApp.swift
+++ dotfiles.swiftpm/MyApp.swift
@@ -1,8 +1,11 @@
 import SwiftUI
 import ComposableArchitecture
+import UserNotifications
 
 @main
 struct MyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
       WindowGroup {
         RootView(
@@ -11,7 +14,29 @@ struct MyApp: App {
                  initialState: AppReducer.State(),
                  reducer: AppReducer()
               )
-        )
+        ).onAppear(perform: {
+            print("appeared xd")
+//            if !UIApplication.shared.isRegisteredForRemoteNotifications {
+                UIApplication.shared.registerForRemoteNotifications()    
+//            }
+        })
       }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("got token!")
+        print(String(data: deviceToken, encoding: .utf8)!)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("failed to get token")
+        print(error.localizedDescription)
+    }
+    
 }
